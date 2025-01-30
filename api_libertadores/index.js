4bef3bf0dc7f304e1052185158bd2c36fce8e72e
BelchiorIsa <anaisabelyribeiro43@gmail.com>
2025-01-30 11:40:27 -0400

    save

---
+++ api_libertadores/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import { retornaCampeonatos } from './servico/retornaCampeonatos_servico.js';
+import { retornaCampeonatosID } from './servico/retornaCampeonatos_servico.js';
+// import pool from './servico/conexao.js';
+
+const app = express();
+ 
+ app.get('/campeonatos', async (req, res) => {
+     const campeonatos = await retornaCampeonatos();
+   res.json(campeonatos)
+ })
+
+ app.get('/campeonatos/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+    const campeonato = await retornaCampeonatosID(id);
+  res.json(campeonato)
+})
+
+app.listen(9000, async () => {
+    const data = new Date();
+    console.log("Servidor node iniciado em: "+data);
+
+    // const conexao = await pool.getConnection();
+    // console.log(conexao.threadId);
+    // conexao.release();
+})
